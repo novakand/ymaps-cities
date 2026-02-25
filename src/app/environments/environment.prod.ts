a5cb110cabdf7cf306280161a65493bd5d4b3536
novakand <a79264799999@gmail.com>
2026-02-26 01:09:56 +0300

    Initial commit

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -4,8 +4,7 @@ export const environment: IEnvironment = {
     debounceTime: 300,
     production: false,
     ssoUri: '',
-    apiUri:'https://artemis.itmontag.keenetic.pro/',
-    apiUriMapbox:'https://api.mapbox.com/',
-    accessTokenMapBox: 'pk.eyJ1Ijoibm92YWthbmQiLCJhIjoiY2p3OXFlYnYwMDF3eTQxcW5qenZ2eGNoNCJ9.PTZDfrwxfMd-hAwzZjwPTg'
+    apiUri: '',
+    apiUriMapbox: '',
+    accessTokenMapBox: ''
 }
-  
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -4,9 +4,9 @@ export const environment: IEnvironment = {
     debounceTime: 300,
     production: false,
     ssoUri: '',
-    apiUri:'https://artemis.itmontag.keenetic.pro/',
-    apiUriMapbox:'https://api.mapbox.com/',
-    accessTokenMapBox: 'pk.eyJ1IjoidHJhY2tjb20xIiwiYSI6ImNtOHBvcmlpMzA0bnEycXNobDI2OWowYTEifQ.QwoWlBurKNAfSDD9UgGB1w',
+    apiUri:'',
+    apiUriMapbox:'',
+    accessTokenMapBox: '',
     baseURL:''
 }
   
